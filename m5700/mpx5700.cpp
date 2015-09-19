5c986616f73a292ae1d6a461de720b2e033469e1
Andrew Plewak <aplewak@buffalo.edu>
2015-09-19 09:54:40 -0700

    source files
    getPsi, getKpa getVac max Psi and maxVac are currently working ,
    setWarnPsi still needs to be configured with hardware

---
+++ m5700/mpx5700.cpp
@@ -0,0 +1,48 @@
+#include "Arduino.h"
+#include "mpx5700.h"
+
+
+int minPsi = 0;
+int minVac = 0;
+int setPsi = 0;
+
+
+int mpx5700::getPsi(int pin){
+	int Pinval = pin;
+	int val = analogRead(Pinval);
+	int psi = val;
+	psi = map(psi,41,940,0,100);
+	return psi;
+}
+int mpx5700::getVac(int pin){		
+    	int Pinval = pin;
+	int val = analogRead(Pinval);
+	int vac = val;
+    	vac = map(vac,30,5,0,30);
+    	return vac;		
+}
+
+
+int mpx5700::maxPsi(int pin){
+	int Pinval = pin;
+	int val = analogRead(Pinval);
+	int psi = val;
+	psi = map(psi,41,940,0,100);
+	if(minPsi < psi){
+		minPsi == psi;
+	}
+	return minPsi;
+}
+int mpx5700::maxVac(int pin){
+	int Pinval = pin;
+	int val = analogRead(Pinval);
+	int vac = val;
+	vac = map(vac,41,940,0,100);
+	if(minVac < vac){
+		minVac == vac;
+	}
+	return minVac;	
+}
+int mpx5700::setWarnPsi(int Pin){
+	return 10;
+}
