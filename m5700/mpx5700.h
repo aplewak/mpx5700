668237a8f29d27d7d4e3376a4cb24eac89d34186
aplewak <andrewplewak@gmail.com>
2016-02-12 08:52:49 -0500

    Update mpx5700.h

--- m5700/mpx5700.h
+++ m5700/mpx5700.h
@@ -1,25 +1,18 @@
 #ifndef mpx5700_h
 #define mpx5700_h
-
 #include "Arduino.h"
 
 class mpx5700
 {
   public:
-
-
 	int pin,vac,psi;
-
 	int getPsi(int Pin);
 	int getVac(int pin);
         int maxPsi(int Pin);
 	int maxVac(int Pinin);
 	int setWarnPsi(int Pin);
 	int input;
-	
-	
   private:
     int _pin;
 };
-
 #endif
